6391ad0e120745bd1c0acadce714f76301e10e4e
lautaro101 <58124032+lautaro101@users.noreply.github.com>
2026-09-19 18:21:18 -0300

    Add files via upload

---
+++ cronometro_de_entrenaminto/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace cronometro_de_entrenaminto
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Cronometro De Descanso");
+            Console.Write("Cuantos segundos de descanso necesitas?: ");
+            int descanso = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            for ( int i= descanso ; i > 0; i--)
+            {
+                Console.WriteLine($"Descansando... {i} seg restantes");
+
+                Thread.Sleep(1000);
+                Console.Clear();
+                
+            }
+            Console.WriteLine("¡Tiempo terminado! ¡A la barra!");
+            Console.Beep();
+
+            Console.ReadKey();
+        }
+    }
+}
