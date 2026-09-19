371babb0c5b25dd8ea86f18db06e59cbc3bbdf41
lautaro101 <58124032+lautaro101@users.noreply.github.com>
2026-09-19 17:57:44 -0300

    Add files via upload

---
+++ Simulador_de_Plazo_Fijo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Versioning;
+
+namespace Simulador_de_Plazo_Fijo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string respuesta;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Simulador De Plazo Fijo");
+                Console.Write("Cuanto Capital Desea Invertir: $");
+                double capital = Double.Parse(Console.ReadLine());
+
+                double ganancia = capital * 0.05;
+
+                double total = capital + ganancia;
+                
+                Console.WriteLine($"Tu Ganancia En 1 Mes Sera De ${ganancia} y Tu Total Sera De ${total}");
+                Console.ReadKey();
+                Console.Clear();
+                Console.Write("¿Desea realizar otra simulación? (si / no): ");
+                
+
+                respuesta = Console.ReadLine();
+            }while (respuesta == "si");
+            Console.Clear();
+            Console.WriteLine("Gracias por usar el simulador");
+
+        }
+        
+    }
+}
