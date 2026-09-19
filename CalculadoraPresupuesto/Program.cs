0f1c33084ab7724c85b35e81665b40744bfe67be
lautaro101 <58124032+lautaro101@users.noreply.github.com>
2026-09-18 21:19:45 -0300

    Add files via upload

---
+++ CalculadoraPresupuesto/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CalculadoraPresupuesto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a su calculadora de presupuesto");
+            Console.WriteLine("Por favor ingrese los siguientes datos.");
+            
+            Console.WriteLine("Presupuesto: ");
+            double presupuesto = double.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("1er  compra: ");
+            string compra1 = Console.ReadLine();
+            Console.WriteLine("Valor de la compra: ");
+            double valor1 = double.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("2da compra: ");
+            string compra2 = Console.ReadLine();
+            Console.WriteLine("Valor de la 2da compra");
+            double valor2 = double.Parse(Console.ReadLine());
+            Console.Clear();
+     
+            
+            double vuelto = presupuesto - (valor1 + valor2);
+
+            double faltante = (valor1 + valor2) - presupuesto;
+
+            if (vuelto >= 0)
+            {
+                Console.WriteLine($"Compra exitosa de {compra1} y {compra2}, aqui tiene su vuelto: {vuelto}");
+                Console.WriteLine("Muchas gracias");
+            }
+
+            else if (faltante > 0)
+            {
+                Console.WriteLine($"Disculpe, te falta esta cantida: {faltante}");
+                Console.WriteLine("Muchas Gracias");
+            }
+
+            
+
+
+
+
+        }
+    }
+}
---
+++ Validador_de_Acceso/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Validador_de_Acceso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BBienvenido al Validador de Acceso de eSports");
+            
+            Console.WriteLine("Cuantos años tenes?:");
+            int edad = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Tiene pase VIP/Competidor: (si / no)");
+            string pase = Console.ReadLine().ToLower();
+            Console.Clear();
+
+            Console.WriteLine("Viene Con Un Tutor: (si / no)");
+            string Tutor = Console.ReadLine().ToLower();
+            Console.Clear();
+
+            if (( edad >= 18 && pase == "si") || (edad < 18 && pase == "si" && Tutor == "si"))
+            {
+                Console.WriteLine("Acceso Concedido, puedes pasar");
+            }
+            else
+            {
+                Console.WriteLine("Acceso Denegado");
+            }
+
+        }
+    
+        
+    }
+}
