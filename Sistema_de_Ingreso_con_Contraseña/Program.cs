8f3cb85457bd49754ace4e2656a32acf789c5849
lautaro101 <58124032+lautaro101@users.noreply.github.com>
2026-09-19 17:17:20 -0300

    Add files via upload

---
+++ Sistema_de_Ingreso_con_Contraseña/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace istema_de_Ingreso_con_Contraseñ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string clavesecreta = "admin123";
+            Console.Write("Ingrese Su Contraseña: ");
+            string contra = Console.ReadLine();
+
+            while (contra != clavesecreta)
+            {
+                Console.WriteLine("Contraseña Incorrecta");
+                Console.ReadKey();
+                Console.Clear();
+                Console.Write("Intente de nuevo: ");
+
+                contra = Console.ReadLine();
+            }
+
+            Console.Clear();
+            Console.WriteLine("¡Acceso concedido! Bienvenido al sistema.");
+
+            Console.ReadKey();
+        }
+    }
+}
