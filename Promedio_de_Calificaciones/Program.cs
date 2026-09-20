b5060008f760d1ff769411460d23b42919df8088
lautaro101 <58124032+lautaro101@users.noreply.github.com>
2026-09-20 01:27:06 -0300

    Add files via upload

---
+++ Promedio_de_Calificaciones/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace Promedio_de_Calificaciones
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            
+            double[] notas = new double[4];
+
+            for(int i = 0; i < 4; i++)
+            {
+                Console.Write($"Ingresar la nota {i + 1}: ");
+
+                notas[i] = double.Parse(Console.ReadLine());
+
+            }
+            Console.Clear();
+            
+            double suma = 0;
+
+            foreach (double nota in notas)
+            {
+                suma = suma + nota;
+            }
+            double promedio = suma / 4;
+            Console.WriteLine($"Tu Promedio final es:{promedio}");
+            Console.ReadKey();
+            
+            
+        }
+    }
+}
---
+++ Registro_de_Series_de_Entrenamiento/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Registro_de_Series_de_Entrenamiento
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double[] pesos = new double[5];
+            for (int i = 0; i < 5; i++ )
+            {
+                Console.Write($"Ingrese el peso levantado en la serie{i + 1}: ");
+
+                pesos[i] = double.Parse(Console.ReadLine());
+            }
+            Console.Clear();
+            Console.WriteLine("--- Resumen de tu Entrenamiento ---");
+            
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Series {i + 1}: {pesos[1]} kg");
+            }
+        }
+    }
+}
