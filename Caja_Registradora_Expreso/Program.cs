7d3144439f68cd4dd8f9412ddae014ec3ba3a663
lautaro101 <58124032+lautaro101@users.noreply.github.com>
2026-09-20 16:54:37 -0300

    Add files via upload

---
+++ Caja_Registradora_Expreso/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+
+namespace Caja_Registradora_Expreso
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string otroCliente;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("--- NUEVO CLIENTE ---");
+
+                double total = 0;
+                double precio;
+
+                do
+                {
+                    Console.Write("Ingrese el precio del producto (0 para terminar): $");
+
+                    precio = double.Parse(Console.ReadLine());
+                    total = total + precio;
+                } while (precio != 0);
+
+                Console.WriteLine($"El total a pagar de : ${total}");
+                Console.WriteLine();
+                Console.Write("¿Hay otro cliente en la fila? (si / no): ");
+
+                otroCliente = Console.ReadLine();
+            } while ((otroCliente == "si")|| (otroCliente == "s"));
+
+            Console.Clear();
+            Console.WriteLine("Caja cerrada. Fin del turno");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sistema_de_Gestión_de_Taller_Metalúrgico/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Sistema_de_Gestión_de_Taller_Metalúrgico
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] materiales = { "electrodos (kg)", "discos de corte", "Perfiles de acero"};
+            int[] stock = { 50, 150, 40};
+
+            bool continuar = true;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("--- SISTEMA DE GESTIÓN DE TALLER ---");
+                Console.WriteLine("1. Ver inventario actual");
+                Console.WriteLine("2. Agregar stock (Llegada de proveedor)");
+                Console.WriteLine("3. Retirar material (Para usar en taller)");
+                Console.WriteLine("4. Salir");
+                Console.Write("Ingrese una opción: ");
+
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        
+                        Console.WriteLine("\n--- INVENTARIO ACTUAL ---");
+
+                        for(int i=0; i < 3; i++)
+                        {
+                        Console.WriteLine($"{(i+1)}. {materiales[1]}: {stock[i]} unidades");
+                        
+                       
+                        }
+                        Console.ReadKey();
+                        break;
+                    
+                    case "2":
+                        Console.WriteLine("\n--- AGREGAR STOCK ---");
+                        
+                        for(int i=0; i < 3; i++)
+                        {
+                        Console.WriteLine($"{(i+1)}. {materiales[i]}: {stock[i]} unidades");
+                        }
+                        Console.Write("Que material desea ingresar? (1-3): ");
+
+                        int ingresar = int.Parse(Console.ReadLine());
+
+                        int indice = ingresar -1;
+
+                        Console.Write("Que cantidad ingreso al taller:");
+                        int cantidad = int.Parse(Console.ReadLine());
+
+                        stock[indice] = stock[indice] + cantidad;
+
+                        Console.WriteLine("¡Stock actualizado con éxito!");
+                        Console.ReadKey();
+                        break;
+                    case "3":
+                        Console.WriteLine("\n--- RETIRAR MATERIAL ---");
+
+                        for (int i = 0; i < 3; i++)
+    {
+        Console.WriteLine($"{(i + 1)}. {materiales[i]}: {stock[i]} unidades");
+    }   
+
+                        
+                }
+            }
+        }
+    }
+}
